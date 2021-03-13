6550ac6e96a9c3cb286ff63dd36fe46499569e9b
JesperKYH <jesper.pettersson@student.kyh.se>
2021-03-13 09:17:02 +0100

    Done with project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# rpsPython" 
+# Rock, Paper, Scissors in Python'
+
+This was a very quick and easy project to get me started on my journey to become a python master
--- rps.py
+++ rps.py
@@ -1,4 +1,40 @@
 from random import randint
 
 #A list of play options
-t = ["Rock", "Paper", "Scissor"];
+t = ["Rock", "Paper", "Scissors"]
+
+#A random play to the computer
+computer =t[randint(0,2)]
+
+#Set player to false
+player = False
+
+while player == False:
+    #Set player to True
+    player = input("Rock, Paper, Scissors?")
+    #Conditions
+    if player == computer:
+        print("tie")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose", computer, "covers", player)
+        else:
+            print("You win", player, "smashes", computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+        else:
+            print("You win!", player, "covers", computer)
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", player)
+        else:
+            print("You win!", player, "cut", computer)
+    #If the player types something thats not Rock, Paper och Scissors
+    else:
+        print("That's not a valid play. Check your spelling!")
+
+    #player was set to True, but we want it to be False so the loop continues
+    player = False
+    computer = t[randint(0,2)]
+
